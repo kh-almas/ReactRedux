b90ac7a7034de866c4d420ce9132200b5bcdec88
kh-almas <services.almas@gmail.com>
2023-09-07 01:00:42 +0600

    use react redux-logger

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import CounterReducer from "./Features/Counter/CounterSlice";
+import logger from 'redux-logger'
 
 const store = configureStore({
     reducer: {
         counter: CounterReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 export default store;
